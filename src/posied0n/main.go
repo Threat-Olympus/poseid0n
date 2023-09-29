5e639c801ec69a4280fbbccdab80eaad8c890431
x3nom <rrohith958@gmail.com>
2023-09-30 00:44:24 +0530

    add: channels to take packets from routine to main

--- src/posied0n/main.go
+++ src/posied0n/main.go
@@ -1,10 +1,18 @@
 package main
 
-import ( 
-	// "fmt"
+import (
+	"fmt"
 	"posied0n/datacollection"
- )
 
- func main(){
-	datacollection.Startpacket()
- }
+	"github.com/google/gopacket"
+)
+
+func main() {
+	channel := make(chan gopacket.Packet)   // Creating channel to write packets to.
+	go datacollection.Startcapture(channel,"wlan0") // Calling Data Collector and passing the channel for writing packets to
+
+	for p := range channel {
+		fmt.Println(p.ApplicationLayer())
+		fmt.Printf("%b\n",(p.ApplicationLayer()).Payload())
+	}
+}
