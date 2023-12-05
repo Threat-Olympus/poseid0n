6c7e9a5f6626af06093417115d4f7d327d39dbc1
x3nom <63553261+x3n0m@users.noreply.github.com>
2023-12-05 23:54:59 +0530

    Changes

--- src/posied0n/main.go
+++ src/posied0n/main.go
@@ -1,30 +1,96 @@
 package main
 
 import (
-	"posied0n/datacollection"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/your-username/myruleengine/ruleengine"
 )
 
 func main() {
-	channel := make(chan gopacket.Packet) // Creating channel to write packets to.
-	output := make(chan string)
-	go datacollection.Startcapture(channel, "wlan0") // Calling Data Collector and passing the channel for writing packets to
+	// Read rules from a file
+	rules, err := readRulesFromFile("rules/example.rules")
+	if err != nil {
+		log.Fatal("Error reading rules:", err)
+	}
 
-	// // for p := range channel {
-	// // 	apppayload := (p.ApplicationLayer()).Payload()
-	// // 	if p.ApplicationLayer() != nil {
-	// // 		fmt.Printf("%b\n",apppayload)
-	// // 	}
+	// Create a RuleEngine with the provided rules
+	engine := ruleengine.NewRuleEngine(rules)
 
-	// // }
-	// //go dataparser.Tcpcheck(channel, output)
-	// // go dataparser.Ipcheck(channel,output)
-	// // for {
-	// fmt.Println(<-output)
-	// // }
-	// go dataparser.LayerCheck(channel, output)
-	// for {
-	// 	fmt.Println(<-output)
-	// }
+	// Open the network interface for packet capture
+	handle, err := pcap.OpenLive("en0", 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	// Process packets from the interface and match against rules
+	for packet := range packetSource.Packets() {
+		// Extract relevant information from the packet
+		port, ip, content := extractPacketInfo(packet)
+
+		// Create a packet string in the expected format for rule matching
+		packetString := fmt.Sprintf("Port:%s IP:%s %s", port, ip, content)
+
+		engine.Match(packetString)
+	}
+}
+
+func extractPacketInfo(packet gopacket.Packet) (string, string, string) {
+	// Extract relevant information from the packet (adjust parsing based on your data format)
+	// For example, assuming the packet contains TCP and IP layers:
+	var port, ip, content string
+
+	if tcpLayer := packet.Layer(gopacket.LayerTypeTCP); tcpLayer != nil {
+		tcp := tcpLayer.(*gopacket.Tcp)
+		port = fmt.Sprintf("%d", tcp.DstPort)
+	}
+
+	if ipLayer := packet.Layer(gopacket.LayerTypeIPv4); ipLayer != nil {
+		ip = ipLayer.(*gopacket.IPv4).SrcIP.String()
+	}
+
+	if appLayer := packet.ApplicationLayer(); appLayer != nil {
+		content = string(appLayer.Payload())
+	}
+
+	return port, ip, content
+}
+
+func readRulesFromFile(filePath string) ([]ruleengine.Rule, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var rules []ruleengine.Rule
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if len(line) > 0 && !strings.HasPrefix(line, "#") {
+			parts := strings.Fields(line)
+			if len(parts) == 3 {
+				port, ip, content := parts[0], parts[1], parts[2]
+				rules = append(rules, ruleengine.Rule{Port: port, IP: ip, Content: content})
+			} else {
+				fmt.Println("Invalid rule format:", line)
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return rules, nil
 }
